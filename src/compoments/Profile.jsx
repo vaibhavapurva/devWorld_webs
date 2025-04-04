6c2b7c143486f690518a539034c03d05e7cdcc6f
Vaibhav Apurva <vaibhavapurvadewas@gmail.com>
2025-04-04 16:22:35 +0530

    commit for changes

--- src/compoments/Profile.jsx
+++ src/compoments/Profile.jsx
@@ -6,37 +6,88 @@ import { BASE_URL } from "../utils/url";
 // import { addUser } from "../utils/userSlice";
 
 const Profile = () => {
-    const user = useSelector(state => state.user)
-//   const dispatch = useDispatch()
-  console.log("hehhehe",user?.firstName)
-  const [firstName ,setFirstName] = useState(user?.firstName)
-  const [LastName ,setLastName] = useState(user?.LastName)
-  const [age ,setAge] = useState(user?.age)
-  const [about ,setAbout] = useState(user?.about)
-  const [gender ,setGender] = useState(user?.gender)
-  const [emailID ,setEmailID] = useState(user?.emailID)
-  const [photoURL, setPhotoURL]= useState(user?.photoURL)
-useEffect(()=>{
-    setFirstName(user?.firstName)
-    setLastName(user?.LastName)
-    setAbout(user?.about)
-    setAge(user?.age)
-    setEmailID(user?.emailID)
-    setGender(user?.gender)
-    setPhotoURL(user?.photoURL)
-},[user])
-    const profileUpdate = async() =>{
-        try{
-            const data = await axios.patch(`${BASE_URL}/profile/edit`,{firstName,LastName,age,about,gender,photoURL},{withCredentials:true})
-            dispatch(addUser(data.data))
-        }catch(err) {
-            console.log(err.message)
-        }
+  const user = useSelector((state) => state.user);
+  //   const dispatch = useDispatch()
+  console.log("hehhehe", user?.firstName);
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [LastName, setLastName] = useState(user?.LastName);
+  const [age, setAge] = useState(user?.age);
+  const [about, setAbout] = useState(user?.about);
+  const [gender, setGender] = useState(user?.gender);
+  const [emailID, setEmailID] = useState(user?.emailID);
+  const [photoURL, setPhotoURL] = useState(user?.photoURL);
+  const [error, setError] = useState(false);
+  const [flag, setFlag] = useState(false);
+  useEffect(() => {
+    setFirstName(user?.firstName);
+    setLastName(user?.LastName);
+    setAbout(user?.about);
+    setAge(user?.age);
+    setEmailID(user?.emailID);
+    setGender(user?.gender);
+    setPhotoURL(user?.photoURL);
+  }, [user]);
+  const profileUpdate = async () => {
+    try {
+      const data = await axios.patch(
+        `${BASE_URL}/profile/edit`,
+        { firstName, LastName, age, about, gender, photoURL },
+        { withCredentials: true }
+      );
+      setFlag(true);
+      setTimeout(() => {
+        setFlag(false);
+      }, 3000);
+      dispatch(addUser(data.data));
+    } catch (err) {
+      console.log(err.message);
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 3000);
     }
+  };
   return (
     <>
+      {flag && (
+        <div role="alert" className="alert alert-info mx-3 my-4 justify-items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="h-6 w-6 shrink-0 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>Profile updated.</span>
+        </div>
+      )}
+       {error && (
+        <div role="alert" className="alert alert-info mx-3 my-4 justify-items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="h-6 w-6 shrink-0 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>Profile can not Updated.</span>
+        </div>
+      )}
       <div className="flex justify-items-center m-9">
         <div className="card bg-base-100 w-96 shadow-xl">
+          {flag && <h1> Profile Update Done</h1>}
           <label className="form-control w-full max-w-xs mx-6">
             <div className="label">
               <span className="label-text">FirstName</span>
@@ -46,7 +97,7 @@ useEffect(()=>{
               value={firstName}
               placeholder="First Name"
               className="input input-bordered w-full max-w-xs"
-              onChange={(e)=> setFirstName(e.target.value)}
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </label>
           <label className="form-control w-full max-w-xs mx-6 m-1">
@@ -56,7 +107,7 @@ useEffect(()=>{
             <input
               type="text"
               value={LastName}
-              onChange={(e)=> setLastName(e.target.value)}
+              onChange={(e) => setLastName(e.target.value)}
               placeholder="Last Name"
               className="input input-bordered w-full max-w-xs"
             />
@@ -68,7 +119,7 @@ useEffect(()=>{
             <input
               type="text"
               value={photoURL}
-              onChange={(e)=> setPhotoURL(e.target.value)}
+              onChange={(e) => setPhotoURL(e.target.value)}
               placeholder="Last Name"
               className="input input-bordered w-full max-w-xs"
             />
@@ -80,7 +131,7 @@ useEffect(()=>{
             <input
               type="text"
               placeholder="Email"
-              onChange={(e)=> setEmailID(e.target.value)}
+              onChange={(e) => setEmailID(e.target.value)}
               value={emailID}
               className="input input-bordered w-full max-w-xs"
             />
@@ -93,7 +144,7 @@ useEffect(()=>{
               type="text"
               placeholder="Age"
               value={age}
-              onChange={(e)=> setAge(e.target.value)}
+              onChange={(e) => setAge(e.target.value)}
               className="input input-bordered w-full max-w-xs"
             />
           </label>
@@ -105,7 +156,7 @@ useEffect(()=>{
               type="text"
               placeholder="Gender"
               value={gender}
-              onChange={(e)=> setGender(e.target.value)}
+              onChange={(e) => setGender(e.target.value)}
               className="input input-bordered w-full max-w-xs"
             />
           </label>
@@ -117,15 +168,20 @@ useEffect(()=>{
               type="text"
               placeholder="About"
               value={about}
-              onChange={(e)=> setAbout(e.target.value)}
+              onChange={(e) => setAbout(e.target.value)}
               className="input input-bordered w-full max-w-xs"
             />
           </label>
           <div className="card-actions flex justify-items-center m-7">
-              <button onClick={profileUpdate} className="btn btn-primary">Update</button>
-            </div>
+            <button onClick={profileUpdate} className="btn btn-primary">
+              Update
+            </button>
+          </div>
         </div>
-        <UserCard user={ {firstName, LastName, age, about, gender,photoURL}} flag={false} />
+        <UserCard
+          user={{ firstName, LastName, age, about, gender, photoURL }}
+          flag={false}
+        />
       </div>
     </>
   );
