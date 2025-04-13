9a8572901b8ddbfffd01c0878866351e8fdd22e2
Vaibhav Apurva <vaibhavapurvadewas@gmail.com>
2025-04-13 14:14:22 +0530

    add path

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,5 +3,9 @@ import { BASE_URL } from "./url";
 
 
 export const createSocketConnection = () =>{
+   if(location.hostname === "Localhost"){
     return io(BASE_URL)
+   }else{
+    return io("/", {path: "/api/socket.io"})
+   }
 }
